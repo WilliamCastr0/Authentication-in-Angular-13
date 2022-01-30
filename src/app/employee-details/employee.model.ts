4c4eb32524f5e21ecb3404181feafdf89ea760af
william_castro <wicastro98@gmail.com>
2022-01-30 15:49:42 -0500

    location-employee-added

--- src/app/employee-details/employee.model.ts
+++
@@ -1,9 +0,0 @@
-export class EmployeeModel{
-    id: number = 0;
-    firstname: string = "";
-    lastname: string = "";
-    enteprise: string = "";
-    address: string = "";
-    telephone: number = 0;
-    email: string = "";
-}
